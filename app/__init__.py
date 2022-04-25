0aa142b5d23989db6089a928b6650ac56b637a1c
EmmaK <emmaculatewkamau@gmail.com>
2022-04-25 14:58:46 +0300

    add movie api

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,6 @@ app = Flask(__name__) #create an app instance.
 
 # Setting up configuration
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 from app import views #imports everything in the views.py file
--- app/views.py
+++ app/views.py
@@ -9,10 +9,11 @@ from app import app
 @app.route('/')  #route decorator
 def index():  #view function
    
-   message = 'Hello World'
+   title = 'Home - Welcome to The best Movie Review Website Online'
    return render_template('index.html')
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
 
-    return render_template('movie.html',id = movie_id)
+   title = 'Movie list'
+   return render_template('movie.html',id = movie_id)
