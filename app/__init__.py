d8c9eb1cf3094415ae64a8b32ba3fea1fa8e186a
EmmaK <emmaculatewkamau@gmail.com>
2022-04-25 16:13:33 +0300

    Got movie list from the TMDB API

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask #import the Flask class from flask module
 from .config import DevConfig
 
 # Initializing application
-app = Flask(__name__) #create an app instance.
+app = Flask(__name__,instance_relative_config = True) #create an app instance.
 
 # Setting up configuration
 app.config.from_object(DevConfig)
--- app/views.py
+++ app/views.py
@@ -4,16 +4,25 @@
 
 from flask import render_template
 from app import app
+from .request import get_movies
 
 # Views
 @app.route('/')  #route decorator
-def index():  #view function
-   
+def index(): #view function
+
+   '''
+   View root page function that returns the index page and its data
+   '''
+
+   # Getting popular movie
+   popular_movies = get_movies('popular')
+   upcoming_movie = get_movies('upcoming')
+   now_showing_movie = get_movies('now_playing')
    title = 'Home - Welcome to The best Movie Review Website Online'
-   return render_template('index.html')
+   return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
 
    title = 'Movie list'
-   return render_template('movie.html',id = movie_id)
+   return render_template('movie.html',title=title, id = movie_id)
